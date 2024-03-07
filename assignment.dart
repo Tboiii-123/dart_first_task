7bd546904686d1753914340e46052f9e06b83a94
Tboiii-123 <132585505+Tboiii-123@users.noreply.github.com>
2024-03-07 22:05:14 +0100

    file commited

---
+++ assignment.dart
@@ -0,0 +1,38 @@
+main(){
+//Datatypes structure
+
+//The integer value is represented by the data type int
+
+int age =10;
+print(age);
+
+//The double value is represented using the data type double
+//It stores numeric values with decimal numbers
+
+double price =50.30;
+print(price);
+
+//The string data type represents its value data type as String with a capital S
+//It represents string literals.and it can be embedded in either single or double quotes.
+
+String name ="Lawal Hussein Taiwo";
+String community ="PLP community";
+print(name);
+print(community);
+
+//The List data type is used to store various data types in a [] square bracket.Its a collection of objects
+//It represents its datatype as List.
+
+List collection =["Lawal","Hussein",1,2.7];
+print(collection);
+
+//Map it represents a set of values as key-value pair.
+//The one enclosed in a string is called the key and the other is called the value
+
+var Ages={
+  "Lawal":12,
+  "Hussein":30,
+  "Taiwo":12
+};
+print(Ages);
+}
